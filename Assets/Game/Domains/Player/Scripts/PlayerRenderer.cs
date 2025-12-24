ef63170895b5d3a1dba0c2018661350c0784647e
Oğuzhan Akbulut <akbulutoguzhan@outlook.com.tr>
2025-12-24 20:18:10 +0300

    feat: if there is no look direction look where we are going

--- Assets/Game/Domains/Player/Scripts/PlayerRenderer.cs
+++ Assets/Game/Domains/Player/Scripts/PlayerRenderer.cs
@@ -9,16 +9,30 @@ namespace Lumenfish.Player
         private static readonly int InputY = Animator.StringToHash("InputY");
 
         [Header("Components")] 
-        public Animator animator;
+        [SerializeField] private Animator animator;
         
         [Header("References")]
-        public Vector2Variable lookDirectionVariable;
+        [SerializeField] private Vector2Variable lookDirectionVariable;
+        [SerializeField] private Vector2Variable moveDirectionVariable;
 
         private void Update()
         {
             var direction = lookDirectionVariable.Value;
+
+            // If we aren't aiming, look where we are going
+            if (direction.sqrMagnitude < 0.01f)
+            {
+                direction = moveDirectionVariable.Value;
+            }
+
+            // If we are neither aiming nor moving, keep the last facing direction (do nothing)
             if (direction.sqrMagnitude < 0.01f) return;
             
+            UpdateAnimator(direction);
+        }
+
+        private void UpdateAnimator(Vector2 direction)
+        {
             var snapX = 0f;
             var snapY = 0f;
             
